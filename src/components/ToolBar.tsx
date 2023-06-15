e6202f41108e16aabc2957d75efd0be368829568
Carlos Martinez <cmartinez98@knights.ucf.edu>
2023-06-15 02:20:17 -0400

    removed button not used on appbar

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -1,10 +1,6 @@
 import Typography from '@mui/material/Typography'
-import Menu from '@mui/material/Menu'
 import Toolbar from '@mui/material/Toolbar'
 import { AccountCircle } from '@mui/icons-material'
-import MenuItem from '@mui/material/MenuItem'
-import Button from '@mui/material/Button'
-import MenuIcon from '@mui/icons-material/Menu'
 
 function ToolBar() {
   return (
@@ -21,38 +17,6 @@ function ToolBar() {
       >
         Travel Knights
       </Typography>
-
-      <Button
-        color='inherit'
-        sx={{ textTransform: 'none' }}
-        // onClick={handleOpenUserMenu}
-      >
-        <MenuIcon />
-      </Button>
-
-      <Menu
-        sx={{ mt: '45px' }}
-        id='menu-appbar'
-        // anchorEl={anchorElUser}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        // temp
-        open={false} // open={Boolean(anchorElUser)}
-        // onClose={handleCloseUserMenu}
-      >
-        <MenuItem
-        //onClick={Logout}
-        >
-          <Typography textAlign='center'>Logout</Typography>
-        </MenuItem>
-      </Menu>
     </Toolbar>
   )
 }
