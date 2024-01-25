072ab5cf1b98ca2bc539a0582c1431908754db6f
Carlos Martinez <carlosmartinezagv@gmail.com>
2024-01-25 18:55:55 -0500

    UPDATE: production url

--- src/redux/user.ts
+++ src/redux/user.ts
@@ -1,6 +1,7 @@
 export const USER = {
   id: 1,
-  name: 'John Doe',
-}
+  name: "John Doe",
+};
 
-export const BASE_URL = 'http://localhost:3001/'
+export const BASE_URL =
+  "https://travelknightsrtqserver-jyz1ukdec-carlosmartinezagv.vercel.app/";
